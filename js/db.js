e95da32f0edcdb648eb39f5a682036d4106565cc
PabloPerdigao <pabloagps@gmail.com>
2026-07-16 12:22:34 -0300

    fix: atualiza configuração do Pool para usar connectionString

--- js/db.js
+++ js/db.js
@@ -1,22 +1,22 @@
-require('dotenv').config(); // variáveis de ambiente
-const { Pool } = require('pg'); 
+require("dotenv").config(); // variáveis de ambiente
+const { Pool } = require("pg");
 
 // Configuração do Pool - conexão com o banco Postgres
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+  connectionString:
+    process.env.DATABASE_URL ||
+    `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
 });
 
 // Teste de conexão
 pool.connect((err, client, release) => {
-    if (err) {
-        return console.error('Erro ao conectar ao banco de dados:', err.stack);
-    }
-    console.log('Banco de dados conectado com sucesso!');
-    release();
+  if (err) {
+    return console.error("Erro ao conectar ao banco de dados:", err.stack);
+  }
+  console.log("Banco de dados conectado com sucesso!");
+  release();
 });
 
 module.exports = pool;
